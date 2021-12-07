125c46fcf544090e93a5977c48256c6be2d97719
sunarya-thito <septogeddon@gmail.com>
2021-12-07 11:12:22 +0700

    Update bungeecord dependency

--- src/test/java/septogeddon/pluginquery/Test.java
+++ src/test/java/septogeddon/pluginquery/Test.java
@@ -1,14 +1,21 @@
 package septogeddon.pluginquery;
 
+import septogeddon.pluginquery.api.QueryConnection;
+
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
 public class Test {
     public static void main(String[]args) {
-        QueryCompletableFuture<String> test = new QueryCompletableFuture<>();
-        test.thenAccept(a -> {
-            System.out.println(a);
-        });
-        test.complete("TEST TEST");
-        test.thenAccept(a -> {
-            System.out.println(a);
-        });
+//QueryConnection proxyConnection;
+//InetAddress targetAddress;
+//proxyConnection.fetchActiveConnections()
+//        .thenAccept(spigotConnections -> {
+//            for (QueryConnection spigotConnection : spigotConnections) {
+//                if (spigotConnection.getAddress().equals(targetAddress)) {
+//                    spigotConnection.sendQuery("myplugin:mymessage", "HELLO".getBytes(StandardCharsets.UTF_8));
+//                }
+//            }
+//        });
     }
 }
